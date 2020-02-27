0127fe1f8f30aefdf9162217c8a73833a35bd78f
pratik.manandhar <pratik.manandhar@fusemachines.com>
2020-02-27 13:37:35 +0545

    implemented winston logger

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,30 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import logger from './shared/utils/logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors();
-  app.disable('x-powered-by');
-  // swagger api documentation for all environments except prod
-  if (process.env.NODE_ENV !== "prod") {
-    const options = new DocumentBuilder()
-      .setTitle("sample")
-      .setDescription("Sample API Documentation")
-      .setVersion('1.0')
-      .addTag('sample')
-      .build();
-    const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup('docs', app, document);
+  try {
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+      logger
+    });
+    app.disable('x-powered-by');
+    // swagger api documentation for all environments except prod
+    if (process.env.NODE_ENV !== "prod") {
+      const options = new DocumentBuilder()
+        .setTitle("sample")
+        .setDescription("Sample API Documentation")
+        .setVersion('1.0')
+        .addTag('sample')
+        .build();
+      const document = SwaggerModule.createDocument(app, options);
+      SwaggerModule.setup('api-docs', app, document);
+    }
+    await app.listen(process.env.PORT || 3000);
+  } catch (error) {
+    
   }
 
-  await app.listen(process.env.PORT || 3000);
+ 
 }
 bootstrap();
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { HttpExceptionFilter } from './http-exception.filter';
-import { LoggerModule } from 'nestjs-pino';
 import { LoggingInterceptor } from './logger.interceptor';
 
 @Module({
-    imports: [
-        LoggerModule.forRoot({
-            useExisting: true,
-        })
-    ],
     providers: [
         {
             provide: APP_FILTER,
@@ -20,6 +14,5 @@ import { LoggingInterceptor } from './logger.interceptor';
             useClass: LoggingInterceptor
         }
     ]
-
 })
 export class SharedModule { }
